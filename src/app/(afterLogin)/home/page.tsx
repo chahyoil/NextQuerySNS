a67c73183defd7b88d4263af20ccdf7504440eea
chahyoil <chahyoil@gmail.com>
2024-09-02 22:21:16 +0900

    [로그인 후 메인] 홈탭 만들면서 Context API 적용해보기

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,26 @@
+import style from './home.module.css'
+import Tab from './_component/Tab'
+import PostForm from './_component/PostForm'
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import Post from '../_component/Post'
+
 export default function Home() {
     return (
-        <main>
-            홈페이지
+        <main className={style.main}>
+            <TabProvider>
+                <Tab/>
+                <PostForm/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+            </TabProvider>
         </main>
     )
 }
